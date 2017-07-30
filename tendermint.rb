ca754ab36dc4fcac17c04aab58e2f581199c9266
Adrian Brink <adrian@brink-holdings.com>
2017-07-30 11:37:59 -0400

    Update tendermint.rb

--- tendermint.rb
+++ tendermint.rb
@@ -12,14 +12,22 @@ class Tendermint < Formula
   depends_on 'glide' => :glide
 
   def install
-    ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
-    system "go", "env" # Debug env
-    system "make", "get_vendor_deps"
-    system "make", "build"
-    bin.install 'build/bin/tendermint'
+      ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
+      ENV["GOPATH"] = buildpath
+      tendermintpath = buildpath/"src/github.com/tendermint/tendermint"
+      tendermintpath.install buildpath.children
+
+      cd tendermintpath do
+          system "go", "env" # Debug env
+          system "glide", "--version" # Debug env
+          system "make", "get_vendor_deps"
+          system "make", "build"
+          bin.install 'build/tendermint'
+      end
   end
 
+
   test do
-    system "#{HOMEBREW_PREFIX}/bin/tendermint", "version"
+      system "#{HOMEBREW_PREFIX}/bin/tendermint", "version"
   end
 end
