e3a75b492c0db02f65a3bca093209806f573f3a9
Anton Kaliaev <anton.kalyaev@gmail.com>
2017-12-04 18:26:53 -0600

    build_from_source flag

--- Formula/ethermint.rb
+++ Formula/ethermint.rb
@@ -1,35 +1,33 @@
-require "formula"
-
 class Ethermint < Formula
-  desc 'Ethereum powered by Tendermint consensus'
-  homepage 'https://github.com/tendermint/ethermint'
-  url 'https://s3-us-west-2.amazonaws.com/tendermint/binaries/ethermint/0.5.3/ethermint_0.5.3_darwin-10.6-amd64.zip'
-  sha256 '75040beb16b8fae6932a893616a2e3f8ee20a2e7957befecb7d7d5fcdf0e5ab9'
-  version '0.5.3'
+  desc "Ethereum powered by Tendermint consensus"
+  homepage "https://github.com/tendermint/ethermint"
+  url "https://s3-us-west-2.amazonaws.com/tendermint/binaries/ethermint/0.5.3/ethermint_0.5.3_darwin-10.6-amd64.zip"
+  version "0.5.3"
+  sha256 "75040beb16b8fae6932a893616a2e3f8ee20a2e7957befecb7d7d5fcdf0e5ab9"
+
+  head do
+    url "https://github.com/tendermint/ethermint.git",
+      :branch => "develop"
+
+    depends_on "go" => :build
+    depends_on "glide" => :optional
+  end
 
   bottle :unneeded
 
   def install
-    if File.exist?('ethermint') # pre-build binary
-      bin.install 'ethermint'
+    if File.exist?("ethermint") # pre-build binary
+      bin.install "ethermint"
     else # build from source
-      ENV['GOPATH'] = buildpath
+      ENV["GOPATH"] = buildpath
       ethermintpath = buildpath/"src/github.com/tendermint/ethermint"
       ethermintpath.install buildpath.children
       cd ethermintpath do
-        system 'make', 'get_vendor_deps'
-        system 'make', 'build'
-        bin.install 'build/ethermint'
+        system "make", "get_vendor_deps"
+        system "make", "build"
+        bin.install "build/ethermint"
       end
     end
-  end
-
-  head do
-    url 'https://github.com/tendermint/ethermint.git',
-      :branch => 'develop'
-
-    depends_on 'go' => :build
-    depends_on 'glide' => :glide
   end
 
   test do
--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -1,36 +1,40 @@
-require "formula"
-
 class Tendermint < Formula
-  desc 'Tendermint Core (BFT Consensus) in Go'
-  homepage 'https://tendermint.com/'
-  url 'https://s3-us-west-2.amazonaws.com/tendermint/binaries/tendermint/v0.12.1/tendermint_0.12.1_darwin_amd64.zip'
-  sha256 '807b29cabc4e372aaee93347f617db8b88bf68c80cb40254c8cd0bd13110d51c'
-  version '0.12.1'
+  desc "Byzantine fault-tolerant replicated state machines in any programming language"
+  homepage "https://tendermint.com/"
+  if ARGV.build_from_source?
+    url "https://github.com/tendermint/tendermint/releases/download/v0.12.1/darwin_amd64.zip"
+    version "0.12.1"
+    sha256 "128e89ec0dcfe5ed86a4effb3cbc1c8e37f09ae9ab5314f097dce4f06618d545"
+  else
+    url "https://s3-us-west-2.amazonaws.com/tendermint/binaries/tendermint/v0.12.1/tendermint_0.12.1_darwin_amd64.zip"
+    version "0.12.1"
+    sha256 "807b29cabc4e372aaee93347f617db8b88bf68c80cb40254c8cd0bd13110d51c"
+  end
+
+  head do
+    url "https://github.com/tendermint/tendermint.git",
+      :branch => "develop"
+
+    depends_on "go" => :build
+    depends_on "glide" => :optional
+  end
 
   bottle :unneeded
 
   def install
-    if File.exist?('tendermint') # pre-build binary
-      bin.install 'tendermint'
-    else # build from source
+    if ARGV.build_from_source? || ARGV.build_head?
       ENV["GOPATH"] = buildpath
       tendermintpath = buildpath/"src/github.com/tendermint/tendermint"
       tendermintpath.install buildpath.children
       cd tendermintpath do
-        system 'make', 'get_vendor_deps'
-        system 'make', 'build'
-        bin.install 'build/tendermint'
+        system "make", "get_vendor_deps"
+        system "make", "build"
+        bin.install "build/tendermint"
         prefix.install_metafiles
       end
+    else
+      bin.install "tendermint"
     end
-  end
-
-  head do
-    url 'https://github.com/tendermint/tendermint.git',
-      :branch => 'develop'
-
-    depends_on 'go' => :build
-    depends_on 'glide' => :glide
   end
 
   test do
